2b5f30a3a6411a9537fa5d3512cf5b8d2eb5d2f9
Egor <epar2002@outlook.com>
2023-04-21 14:05:07 +0300

    ResolutionCheck

---
+++ Assets/Scripts/Utils/UIPositionConverter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public class UIPositionConverter
+    {
+        private readonly Canvas _canvas;
+        private readonly Vector2 _targetResolution;
+    }
+}
